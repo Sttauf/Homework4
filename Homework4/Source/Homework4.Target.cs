e97dd400bb2d0bb27570a553239f87beec3f987a
Sarah Taufner <sttauf@yahoo.com.br>
2020-11-04 10:08:43 -0500

    +UpdatedFiles

---
+++ Homework4/Source/Homework4.Target.cs
@@ -0,0 +1,13 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Homework4Target : TargetRules
+{
+	public Homework4Target(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		ExtraModuleNames.Add("Homework4");
+	}
+}
---
+++ Homework4/Source/Homework4/Homework4.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Homework4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Homework4, "Homework4" );
+ 
